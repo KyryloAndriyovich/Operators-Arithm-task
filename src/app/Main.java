1609b8d98b32660de8909c00036c8b1642932e91
KyryloAndriyovich <143022068+KyryloAndriyovich@users.noreply.github.com>
2023-08-27 00:09:22 +0300

    first add

---
+++ src/app/Main.java
@@ -0,0 +1,38 @@
+package app;
+
+public class Main {
+
+    static String name;
+    static int quota;
+    static double price;
+    static double salesAmount;
+    static double avgSum;
+    static int days;
+
+    public static void main(String[] args) {
+
+        name = "Oranges";
+        quota = 158;
+        price = 2.99;
+        days = 4;
+
+
+        salesAmount = quota * price;
+        System.out.println("Product: " + name + ", sales EUR " + salesAmount);
+
+        avgSum = salesAmount / days;
+        System.out.println("Product: " + name + ", sales by day EUR " + String.format("%.2f", avgSum));
+
+        name = "Apples";
+        quota = 117;
+        price = 1.95;
+        days = 7;
+
+        salesAmount = quota * price;
+        System.out.println("Product: " + name + ", sales EUR " + salesAmount);
+
+        avgSum = salesAmount / days;
+        System.out.println("Product: " +  name + ", sales by day EUR " + String.format("%.2f", avgSum));
+
+    }
+}
